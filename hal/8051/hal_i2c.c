3db99365430d2783df7ad91258e4fc788b402f39
iamlvshijie <iamlvshijie@hotmail.com>
2016-11-04 13:31:51 +0800

    add 8051

---
+++ hal/8051/hal_i2c.c
@@ -0,0 +1,7 @@
+/*  @file:
+	@author:
+	@revision:
+	@history:
+	@copyright:
+*/
+#include "hal_i2c.h"
---
+++ hal/8051/template.bat
@@ -0,0 +1,25 @@
+echo 
+echo hal_template generating...
+
+
+cd.>hal_i2c.h
+echo /*  @file:>>hal_i2c.h
+echo 	@author:>>hal_i2c.h
+echo 	@revision:>>hal_i2c.h
+echo 	@history:>>hal_i2c.h
+echo 	@copyright:>>hal_i2c.h
+echo */>>hal_i2c.h
+echo #include "include.h">>hal_i2c.h
+
+cd.>hal_i2c.c
+echo /*  @file:>>hal_i2c.c
+echo 	@author:>>hal_i2c.c
+echo 	@revision:>>hal_i2c.c
+echo 	@history:>>hal_i2c.c
+echo 	@copyright:>>hal_i2c.c
+echo */>>hal_i2c.c
+echo #include "hal_i2c.h">>hal_i2c.c
+
+
+
+pause
